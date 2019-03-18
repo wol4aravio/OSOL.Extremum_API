9eef51e03dd585418d5f0aa7cd8474448800fde8
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-03-18 16:36:06 +0300

    test

--- src/main/scala/osol/extremum/api/EntryPoint.scala
+++ src/main/scala/osol/extremum/api/EntryPoint.scala
@@ -3,16 +3,17 @@ import org.rogach.scallop._
 import cats.effect.IO
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response}
-import com.twitter.finagle.http.filter.JsonpFilter
 import com.twitter.util.Await
 import io.finch._
 import io.finch.catsEffect._
 import io.finch.circe._
 import io.circe.generic.auto._
 
+import osol.extremum.api.Functions._
+
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-  val port = opt[Int](default = Some(8080), validate = (0<))
+  val port = opt[Int](default = Some(8080), validate = (0 <))
   verify()
 }
 
@@ -24,11 +25,18 @@ object Main extends App {
     Ok("OK")
   }
 
-  def service: Service[Request, Response] = JsonpFilter
-    .andThen(
-      Bootstrap
-        .serve[Text.Plain](healthCheck)
-        .toService)
+  def sphereFunctionInfo: Endpoint[IO, Map[String, Any]] = get("sphere" :: path[String] :: "info") {
+    n_dim_str: String => {
+      val f = new SphereFunction(n_dim_str.toInt)
+      Ok(Map("name" -> f.name, "n_dim" -> f.n_dim, "search_area" -> f.search_area, "x_optimal" -> f.x_optimal))
+    }
+  }
+
+  def service: Service[Request, Response] =
+    Bootstrap
+      .serve[Text.Plain](healthCheck)
+      .serve[Application.Json](sphereFunctionInfo)
+      .toService
 
   override def main(args: Array[String]): Unit = {
     val conf = new Conf(args)
