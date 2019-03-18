37b8389aaace9d3614c663bcfa00ae3b9c405308
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-03-18 16:35:47 +0300

    Sphere Function

--- src/main/scala/osol/extremum/api/Functions.scala
+++ src/main/scala/osol/extremum/api/Functions.scala
@@ -3,13 +3,17 @@ package osol.extremum.api
 object Functions {
 
   abstract class OptimizationBenchmark {
+    val name: String
+    val n_dim: Int
     val search_area: List[(Double, Double)]
     val x_optimal: List[Double]
 
     def calculate(x: List[Double]): Double
   }
 
-  class SphereFunction(n_dim: Int) extends OptimizationBenchmark {
+  class SphereFunction(dimensionality: Int) extends OptimizationBenchmark {
+    val name = "SphereFunction"
+    val n_dim = dimensionality
     val search_area: List[(Double, Double)] = (1 to n_dim).map(_ => (-25.0, 25.0)).toList
     val x_optimal: List[Double] = List.fill[Double](n_dim)(0.0)
 
